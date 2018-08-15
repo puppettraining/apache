d5272bf84586371552e01c7e4506189842be2627
root <root@puppetmaster.local>
2018-08-15 22:54:07 +0000

    correct submodule name

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,4 +1,4 @@
-class apache::package (
+class apache::service (
         String $servicename = lookup({"name" => "apache.servicename", "default_value" => "default"})
         ) {
         service { 'webserver-service':
